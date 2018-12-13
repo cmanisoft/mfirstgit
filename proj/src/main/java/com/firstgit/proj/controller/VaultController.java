8c9c785b90c18a9396c72f820b7a92276f2febc4
maniarea <maniarea@gmail.com>
2018-12-13 13:31:07 +0530

    change1

--- proj/src/main/java/com/firstgit/proj/controller/VaultController.java
+++ proj/src/main/java/com/firstgit/proj/controller/VaultController.java
@@ -1,5 +1,13 @@
 package com.firstgit.proj.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/vault")
 public class VaultController {
+	
+	
+	
 
 }
