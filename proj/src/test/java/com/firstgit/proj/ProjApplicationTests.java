d6d77cc67191ab73e4032ec60ce2faed3bba9876
maniarea <maniarea@gmail.com>
2018-12-13 12:52:28 +0530

    fist comm

---
+++ proj/src/test/java/com/firstgit/proj/ProjApplicationTests.java
@@ -0,0 +1,17 @@
+package com.firstgit.proj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
